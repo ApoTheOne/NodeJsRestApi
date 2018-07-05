c52bbaaeadfe9227277b3572c1d58f0814e127ed
Saklani <saklania@travelex.com>
2018-07-05 16:51:17 +0530

    Added samples for GET, POST, PUT, DELETE via http module

--- index.js
+++ index.js
@@ -1,12 +1,52 @@
 let http = require('http');
 var port = process.env.WEBPORT;
 
-function handleRequest(request, response) {
+function handleGetRequest(request, response) {
+	response.writeHead(200, {
+		'Content-Type' : 'text/plain'
+	});
+	response.end('Get action request.');
+}
+
+function handlePostRequest(request, response) {
     response.writeHead(200, {
         'Content-Type': 'text/plain'
     });
-    response.end('Response ended');
-    console.log('server responded.');
+    response.end('Post action request.');
+}
+
+function handlePutRequest(request, response) {
+    response.writeHead(200, {
+        'Content-Type': 'text/plain'
+    });
+    response.end('Put action request.');
+}
+
+function handleDeleteRequest(request, response) {
+    response.writeHead(200, {
+        'Content-Type': 'text/plain'
+    });
+    response.end('Delete action request.');
+}
+
+function handleRequest(request, response) {
+	switch (request.method) {
+		case 'GET:
+			handleGetRequest(request, response);
+			break;
+		case 'POST':
+			handlePostRequest(request, response);
+			break;
+		case 'PUT:
+			handlePutRequest(request, response);
+			break;
+		case 'DELETE':
+			handleDeleteRequest(request, response);
+			break;
+		default: handleBadRequest(request, response);
+			break;
+	}
+	console.log('Request processing completed');
 }
 
 http.createServer(handleRequest).listen(port, '127.0.0.1');
